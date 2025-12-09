07ec3b94caffa1cc77c4e4e2f0501bb676bc2f0c
Samuel Copa <samuel14.example.com>
2025-12-09 13:33:28 +0100

    Realizo el scaffold de scf_incidencias además de el primer modulo, con sus 2 primeras vistas

---
+++ odoo/addons/scf_incidencias/models/scf_incidencias_issues.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class ScfIncidenciasIssues(models.Model):
+    _name = 'scf_incidencias.issues'
+    _description = 'Incidencias'
+
+    name = fields.Char(string='Título', required=True, help="Introduce el título de la incidencia")
+    description = fields.Text(string='Descripción', help="Descripción detallada")
+    
+    priority = fields.Selection([
+        ('0', 'Baja'),
+        ('1', 'Media'),
+        ('2', 'Alta')
+    ], string='Prioridad', default='0')
+    
+    date_reported = fields.Date(
+        string='Fecha de Reporte',
+        default=fields.Date.context_today
+    )
+
+    state = fields.Selection([
+        ('draft', 'Nueva'),
+        ('process', 'En Proceso'),
+        ('done', 'Resuelta')
+    ], string='Estado', default='draft')
